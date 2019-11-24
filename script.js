e32984420aa8b1635d04f6a7e4bd3572d1df73ad
Greg Roberts <groberts3104@gmail.com>
2019-11-24 18:09:46 -0500

    added div for weather info

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ $(document).ready(function () {
     $scope.moment = moment($scope.date);
     var hr = "h";
     var time = "h:mm:ss";
-    
+
 
 
     // DateTime Format
     $('#currentDay').html($scope.moment.format('MMMM Do h:mm:ss a'));
-    
+
     $(searchBtnEl).click(function () {
+        event.preventDefault();
         var inputVal = $("#inputInfo").val();
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/weather?q=" + inputVal + "id=524901&APPID=982e2fe78532143c4bbf9987ca2a49cc",
@@ -24,6 +25,13 @@ $(document).ready(function () {
             console.log("Temperature: " + response.main.temp);
             console.log("Humidity: " + response.main.humidity);
             console.log("Wind Speed: " + response.wind.speed);
+            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+            $(".temp").text("Temperature (F) " + response.main.temp);
+            $(".humidity").text("Humidity: " + response.main.humidity);
+            $(".wind").text("Wind Speed: " + response.wind.speed);
+
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            $(".tempF").text("Temperature (F) " + tempF);
         })
     });
 })
